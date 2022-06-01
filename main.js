bc8f5ae89d4d2492a81731e3ffee7a4e567bed69
masud1989 <mranasrj@gmail.com>
2022-06-01 17:59:28 +0600

    url-module

--- main.js
+++ main.js
@@ -1,30 +1,23 @@
 const http = require('http');
+const URL = require('url');
 
 const server = http.createServer(function (req, res) {
 
-    if(req.url == '/'){
-        res.writeHead(200, {'content':'text/html'})
-        res.write(`<h1>This is home Page</h1>`)
-        res.end();
-    }
+   const myURL = 'http://reb.teletalk.com.bd/login.php?msg=Please%20login%20with%20your%20username%20and%20password';
+   const urlObject = URL.parse(myURL, true);
 
-    else if(req.url == '/about'){
-        res.writeHead(200, {'content':'text/html'})
-        res.write(`<h1>This is about Page</h1>`)
-        res.end();
-    }
+    const myHostName = urlObject.host;
+    const mypathName = urlObject.pathname;
+    const mySearchName = urlObject.search;
 
-    else if(req.url == '/contact'){
-        res.writeHead(200, {'content':'text/html'})
-        res.write(`<h1>This is contact Page</h1>`)
-        res.end();
-    }
-    
-    else if(req.url == '/test'){
-        res.writeHead(200, {'content':'text/html'})
-        res.write(`<h1>This is test Page</h1>`)
-        res.end();
-    }
+    res.writeHead( 200, {'content-type': 'text/html'})
+    // res.write(myHostName);
+    // res.write(mypathName);
+    res.write(mySearchName);
+    res.end();
+
+
+
 });
 
 server.listen(5050)
